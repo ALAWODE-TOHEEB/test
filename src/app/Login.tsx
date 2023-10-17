a90f4ffe723755e6b0e6a857b0adccc85bfa5dfe
ALAWODE-TOHEEB <alawodetoheeb8@gmail.com>
2023-10-17 16:10:37 -0700

    first commit

---
+++ src/app/Login.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      hey
+    </div>
+  )
+}
+
+export default login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,146 @@
-import Image from 'next/image'
-
+"use client";
+import Image from "next/image";
+import WorkpaLogo from ".//WokpaLogo.png";
+import Waveform from ".//waveform.png";
+import star from ".//stars.png";
+import badge from ".//badge.png";
+import microphone from ".//microphone.png";
+import heart from ".//heart.png";
+import list from ".//list.png";
+import plus from ".//plus.png";
+import vector from ".//Vector.png";
+import playlist from ".//playlist.png";
+import recent from ".//recent.png";
+import subcription from ".//subscription.png";
+import podcast from ".//podcast.png";
+import advert from ".//advert.png";
+import blog from ".//blog.png";
+import mobile from ".//mobile.png";
+import search from ".//search.png";
+import star1 from ".//star1.png";
+import Sidebar from "./Sidebar";
+import { useState } from "react";
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex ">
+      <div className="bg-zinc-800 w-1/4 h-screen p-4 overflow-auto">
+        <Image src={WorkpaLogo} alt="" />
+
+        <div className="py-5">
+          <div className="text-white flex-col space-y-5 text-lg font-bold mb-4">
+            <div className="flex  gap-1">
+              <Image src={Waveform} alt="" /> <p className="">Discover</p>
+            </div>
+            <div className="flex  gap-1">
+              <Image src={star} alt="" /> <p className="">Top 50 podcasts</p>
+            </div>
+            <div className="flex  gap-1">
+              <Image src={badge} alt="" /> <p className="">Categories</p>
+            </div>
+            <div className="flex  gap-1">
+              <Image src={microphone} alt="" />{" "}
+              <p className="">Add your podcast</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="cursor-pointer flex justify-center items-center gap-2 "
+          onClick={toggleDropdown}
+        >
+          <p>Your Library</p> <Image className="w-2 h-2" src={vector} alt="" />
+        </div>
+        <div className="flex justify-center items-center cursor-pointer mb-16">
+          {isOpen && (
+            <div className=" w-32 p-4 flex justify-center items-center gap-3 ">
+              <div>
+                <Image src={heart} alt="" />
+              </div>
+              <div>
+                <Image src={list} alt="" />
+              </div>
+              <div>
+                <Image src={plus} alt="" />
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="text-white flex-col space-y-5 text-lg font-bold mb-4">
+          <div className="flex  gap-1">
+            <Image src={playlist} alt="" /> <p className="">Your playlist</p>
+          </div>
+          <div className="flex  gap-1">
+            <Image src={recent} alt="" /> <p className="">Recently played</p>
+          </div>
+          <div className="flex  gap-1">
+            <Image src={subcription} alt="" />{" "}
+            <p className="">Subscription & Hosts</p>
+          </div>
+        </div>
+
+        <hr className="mb-4" />
+
+        <div className="text-white flex-col space-y-5 text-lg font-bold mb-4">
+          <div className="flex  gap-1">
+            <Image src={blog} alt="" /> <p className="">Wokpa for podcasters</p>
+          </div>
+          <div className="flex  gap-1">
+            <Image src={recent} alt="" />{" "}
+            <p className="">Wokpa for advertisers</p>
+          </div>
+          <div className="flex  gap-1">
+            <Image src={subcription} alt="" />{" "}
+            <p className="">Read our blogs</p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-neutral-800 w-3/4 h-16 p-4">
+        <nav className="flex justify-between">
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search by podcast Name"
+              className="pl-10 pr-2 py-1 rounded-md w-full"
+            />
+            <Image
+              src={search}
+              alt=""
+              className="absolute right-2 top-1/2 bottom-1/2 transform -translate-y-1/2 w-5 h-5"
+            />
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="w-[197px] h-[42px] bg-black rounded-[18px] shadow flex justify-center items-center">
+              <Image src={mobile} alt="" />
+              <button className="">Download the app</button>
+            </div>
+            <button className="w-[120px] h-[42px] bg-teal-500 rounded-xl">
+              Signup
+            </button>
+            <button className="w-[120px] h-[42px] rounded-xl border border-white">
+              Login
+            </button>
+          </div>
+        </nav>
+
+        <div className=" bg-neutral-800">
+          <div className="p-5">
+            <p>
+              <Image src={star1} alt="" />{" "}
+              <div className="text-white text-[28px] font-bold font-['Euclid Circular A'] leading-tight">
+                Pick of the week
+              </div>
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
